6060ebf9d95455994dadfbc785db54f3cc58e289
Ethan Stillman <ehs.stillman@gmail.com>
2021-10-06 14:02:27 -0700

    Rollup / Babel are working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
 
 import Arweave from 'arweave';
 
+// Don't need to specify this in newer versions of the package,
+// but doing so here for learning
 const arweave = Arweave.init({
     host: 'arweave.net',
     protocol: 'https',
     port: 443
 });
 
+// this won't work -- need to provide `wallet` and `contractSource`
+async function createContract() {
+    // Create the contract transaction
+    const contractTx = await arweave.createTransaction(
+        { data: contractSource },
+        wallet
+    );
+    contractTx.addTag('App-Name', 'SmartWeaveContractSource');
+    contractTx.addTag('App-Version', '0.1.0');
+    contractTx.addTag('Content-Type', 'application/javascript');
 
+    // Sign transaction
+    await arweave.transactions.sign(contractTx, wallet);
+
+    // Let's keep the ID, it will be used in the state transaction
+    const constractSourceTxId = contractTx.id;
+
+    // Deploy the contract source
+    await arweave.transactions.post(contractTx);
+
+
+    // Now, let's create the Initial State transaction
+    const initialStateTx = await arweave.createTransaction(
+        { data: initialState },
+        wallet
+    )
+    initialStateTx.addTag('App-Name', 'SmartWeaveContractSource');
+    initialStateTx.addTag('App-Version', '0.1.0');
+    initialStateTx.addTag('Contract-Src', contractSourceTxId);
+    initialStateTx.addTag('Content-Type', 'application/json');
+
+    // Sign transaction
+    await arweave.transactions.sign(initialStateTx, wallet);
+    const initialStateTxId = initialStateTx.id;
+
+    // Deploy
+    await arweave.transactions.post(initialStateTx);
+}
+createContract();
 
 
 export function handle(state, action) {
